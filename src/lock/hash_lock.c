061eded32ba8a73de8a18d61d8c2c97441e6fff3
hahaha <a310608318@gmail.com>
2025-03-11 20:17:02 +0800

    3.11

--- src/lock/hash_lock.c
+++ src/lock/hash_lock.c
@@ -3,18 +3,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 void hashInit(hash_lock_t* bucket) {
-  perror("This function is not implemented");
+  for(int i=0;i<HASHNUM;i++){
+    bucket->table[i].head=(Hlist)malloc(sizeof(Hnode));
+    pthread_mutex_init(&(bucket->table[i].mutex),NULL);
+  }
 }
 
 int getValue(hash_lock_t* bucket, int key) {
-  perror("This function is not implemented");
+  int index=HASH(key);
+  pthread_mutex_lock(&(bucket->table[index].mutex));
+  Hnode* node=bucket->table[index].head;
+  while(node->next!=NULL){
+    if(node->key==key){
+      return node->value;
+    }
+    node=node->next;
+  }
+  pthread_mutex_unlock(&(bucket->table[index].mutex));
 }
 
 void insert(hash_lock_t* bucket, int key, int value) {
-  perror("This function is not implemented");
+  int index=HASH(key);
+  pthread_mutex_lock(&(bucket->table[index].mutex));
+  Hnode*node=bucket->table[index].head;
+  while(node->next!=NULL){
+    if(node->key==key){
+      node->key=key;
+      return ;
+    }
+    node=node->next;
+  }
+  Hnode* new_node=(Hlist)malloc(sizeof(Hnode));
+  node->next=new_node;
+  new_node->next=NULL;
+  new_node->key=key;
+  new_node->value=value;
+  pthread_mutex_unlock(&(bucket->table[index].mutex));
+  return ;
 }
 
 int setKey(hash_lock_t* bucket, int key, int new_key) {
-  perror("This function is not implemented");
+  int index=HASH(key);
+  int new_index=HASH(new_key);
+  Hnode*node=bucket->table[index].head;
+  Hnode*new_node=bucket->table[index].head;
+  int value=0;
+  while(node->next!=NULL){
+    if(node->key==key){
+      value=node->value;
+      insert(bucket,new_key,value);
+      return 0;
+    }
+    node=node->next;
+  }
+  return -1;
 }
--- src/lock/list_lock.c
+++ src/lock/list_lock.c
@@ -3,18 +3,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 void listInit(list_lock_t* list) {
-   perror("This function is not implemented");
+  list->head=(LinkList)malloc(sizeof(LNode));
+  pthread_mutex_init(&(list->mutex),NULL);
+  pthread_cond_init(&(list->cond),NULL);
 }
 
 void producer(list_lock_t* list, DataType value) {
-  perror("This function is not implemented");
+  pthread_mutex_lock(&(list->mutex));
+  LNode* temp=list->head;
+  while(temp->next!=NULL){
+    temp=temp->next;
+  }
+  LNode* new_node=(LinkList)malloc(sizeof(LNode));
+  temp->next=new_node;
+  new_node->next=NULL;
+  new_node->value=value;
+  pthread_mutex_unlock(&(list->mutex));
+  pthread_cond_signal(&(list->cond));
 }
 
 void consumer(list_lock_t* list) {
-   perror("This function is not implemented");
+  if(list->head->next==NULL){
+    pthread_cond_wait(&(list->cond),&(list->mutex));
+  }
+  LNode* temp1=list->head;
+  LNode* temp2=list->head;
+  while(temp1->next!=NULL){
+    temp2=temp1;
+    temp1=temp1->next;
+  }
+  temp2->next=NULL;
+  free(temp1);
 }
 
 int getListSize(list_lock_t* list) {
-  perror("This function is not implemented");
+  LNode* temp=list->head;
+  int count=0;
+  while(temp->next!=NULL){
+    temp=temp->next;
+    count++;
+  }
+  return count;
 }
